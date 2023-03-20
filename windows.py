a1584407b4855b46f65d3824b6b313dc307043d8
Thejairex <Yairjesus777@gmail.com>
2023-03-20 20:09:44 -0300

    Init project

---
+++ windows.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+import numpy as np
+import os
+from time import time
+from windowsCapture import WindowCapture
+
+# Change the working directory to the folder this script is in.
+# Doing this because I'll be putting the files from each video in their own folder on GitHub
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+# initialize the WindowCapture class
+wincap = WindowCapture('Forager By HopFrog')
