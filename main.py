b4fcaa61dd61b2f39854e1576f137becb179189b
Thejairex <Yairjesus777@gmail.com>
2023-03-21 20:44:05 -0300

    Add windows to combobox and new frame panel

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from pywinauto import Desktop
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
-
 class Aplicacion(ttk.Frame):
     """
 
@@ -17,6 +15,7 @@ class Aplicacion(ttk.Frame):
     """
     def __init__(self, master=None):
         super().__init__(master)
+        self.state_panelControl = False
         self.master = master
 
         for index in [0, 1, 2]:
@@ -31,13 +30,19 @@ class Aplicacion(ttk.Frame):
         Renueva la lista de programas abiertos para ser insertados en la consola.
 
         """
-        windows = Desktop(backend="uia").windows()
+        windowsTemp = Desktop(backend="uia").windows()
         self.txt_consola.config(state='normal')
+        self.txt_consola.delete(1.0, 'end')
         self.txt_consola.insert(tk.END, "Estos son los programas que tenes abiertos: \n")
-        self.btn_buscar.config(text='Actualizar')
-        for window_text  in [w.window_text() for w in windows]:
+        self.btn_buscar.config(text = 'Actualizar')
+        windows = [w.window_text() for w in windowsTemp]
+        windows.append('Fullscreen')
+
+        for window_text  in windows:
             self.txt_consola.insert(tk.END, f" • {window_text}\n")
 
+        self.txt_consola.focus_set()
+        self.cbb_window.config(values=windows)
         self.txt_consola.config(state='disabled')
 
     def widgets(self):
@@ -46,6 +51,20 @@ class Aplicacion(ttk.Frame):
         Diseño estetico de el programa con tkinter
 
         """
+        def abrirPanelControl(event):
+            if not self.state_panelControl:
+                print('Se habri el panel de control')
+
+                self.frm_panelControl.grid(
+                    row = 0, column = 1, padx=(20, 10), pady=(20, 10), sticky="nsew"
+                    )
+                ttk.Button(self.frm_panelControl, text='botton', command='').grid(
+                    row=0, column=0, padx=5, pady=10, sticky="nsew"
+                )
+                self.state_panelControl = True
+
+        self.frm_panelControl = ttk.LabelFrame(self.master, text='Panel', padding=(20,10))
+
         self.frm_lista = ttk.LabelFrame(self.master, text='Visor' ,padding=(20,10))
         self.frm_lista.grid(
             row = 0, column = 0, padx=(20, 10), pady=(20, 10), sticky="nsew"
@@ -56,10 +75,20 @@ class Aplicacion(ttk.Frame):
             row=0, column=0, padx=5, pady=10, sticky="nsew"
         )
 
+        ttk.Label(self.frm_lista,text='Seleccionar programa:').grid(
+            row=1, column=0, padx=5, pady=10, sticky="nsew"
+            )
 
+        self.cbb_window = ttk.Combobox(self.frm_lista, values=[], state='readonly')
+        self.cbb_window.bind("<<ComboboxSelected>>", abrirPanelControl)
+        self.cbb_window.grid(
+            row=2, column=0, padx=5, pady=10, sticky="nsew"
+            )
+
+    #********** Textbox Consola ***********
         self.txt_consola = tk.Text(self.frm_lista, width=60, height=8, state='disabled')
         self.txt_consola.grid(
-            row=1, column=0, padx=5, pady=10, sticky="nsew"
+            row=3, column=0, padx=5, pady=10, sticky="nsew"
         )
 
         for row in range(10):
@@ -70,7 +99,7 @@ class Aplicacion(ttk.Frame):
         scrollbar_y = ttk.Scrollbar(
             self.frm_lista, orient="vertical", command=self.txt_consola.yview
             )
-        scrollbar_y.grid(row=1, column=1, sticky="ns")
+        scrollbar_y.grid(row=3, column=1, sticky="ns")
         self.txt_consola.config(yscrollcommand=scrollbar_y.set)
 
 
@@ -78,6 +107,8 @@ class Aplicacion(ttk.Frame):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Capturadora")
+
+    # Tema Azure descargado de 'https://github.com/rdbende/Azure-ttk-theme'
     root.tk.call("source", "azure.tcl")
     root.tk.call("set_theme", "dark")
 
